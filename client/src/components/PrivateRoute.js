babb0607f51371ed853d819e4d7d115ce1fb1a55
aegucer <43250844+emregucerr@users.noreply.github.com>
2023-11-23 13:27:20 -0600

    Implemented PrivateRoute component, replaced regular Route components with PrivateRoute for private pages, and removed login check and redirect logic from Homepage and ProfilePage components.

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React, { useState, useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useContext(UserContext);
+  
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+          <Redirect to={{
+            pathname: '/login',
+            state: { from: location }
+          }}/>
+        )
+      }
+    />
+  );
+};
+export default PrivateRoute;
