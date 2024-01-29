56af7d7b4dd84c97f221431e77298fe4d76a49c5
FumeDev <FumeDev@users.noreply.github.com>
2024-01-29 08:19:57 -0800

    Refactored App.js to remove inline routes and created a separate component for routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import { UserContextProvider } from "./contexts/UserContext";
  *To avoid bloated App.js component, page routes created at Routes.js component.
  */
 
- // This is a change made by a human - one of the last of its kinds version 2
-
 function App() {
   return (
     <>
